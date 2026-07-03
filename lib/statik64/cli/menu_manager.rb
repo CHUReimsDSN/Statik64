f08b38c9b0fe56b33bb5bf421e4797d1060b86ab
JulesDebeaumont <deb.jules@outlook.fr>
2026-07-03 16:19:49 +0200

    [MORE]

--- lib/statik64/cli/menu_manager.rb
+++ lib/statik64/cli/menu_manager.rb
@@ -29,6 +29,7 @@ module Statik64
                 
                 run_menu(MENU_HOME_NAME)
                 while self.actions_pool.any?
+                    self.actions_pool.count
                     first_action = self.actions_pool.first
                     self.actions_pool.unshift
                     first_action.call
